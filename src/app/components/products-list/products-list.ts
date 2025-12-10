9dc4b1ee7eaf147d103919d22f903e10af2aaad8
Ander Vilariño <andervlrn@gmail.com>
2025-12-10 19:29:13 +0100

    Creacion del componente product-card

--- src/app/components/products-list/products-list.ts
+++ src/app/components/products-list/products-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Product, ProductService } from "../../services/product";
+import { ProductCard } from '../product-card/product-card';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './products-list.html',
   styleUrl: './products-list.css',
 })
