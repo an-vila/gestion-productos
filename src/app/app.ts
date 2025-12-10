c1f8510bc5ebcd065faf4f096969b93d4fae1055
Ander Vilariño <andervlrn@gmail.com>
2025-12-10 18:35:02 +0100

    Creacion de lista de productos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { Product, ProductService } from "./services/product";
+import { ProductsList } from "./components/products-list/products-list";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, ProductsList],
 	templateUrl: "./app.html",
 })
 
---
+++ src/app/components/products-list/products-list.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Product, ProductService } from "../../services/product";
+
+@Component({
+  selector: 'app-products-list',
+  imports: [],
+  templateUrl: './products-list.html',
+  styleUrl: './products-list.css',
+})
+export class ProductsList {
+
+  productos: Product[] = [];
+
+  constructor (private productService: ProductService){
+    this.productService.cargarProductos().subscribe(
+      (datos) => {
+        this.productos = datos;
+        console.log('Productos recibidos', datos);
+      }
+    );
+  }
+}
